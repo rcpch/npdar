2e28c4998aa4fb489d30f985e6cf9c67e774df48
ZhaonanFang <zhaonan.fang@rcpch.ac.uk>
2026-02-27 19:36:37 +0000

    template for BP categories & references

---
+++ man-roxygen/bp_refs.R
@@ -0,0 +1,11 @@
+# Common references for BP functions
+#' @references
+#' \itemize{
+#'   \item NHBPEP Fourth Report: \href{https://www.nhlbi.nih.gov/files/docs/resources/heart/hbp_ped.pdf}{Paediatric BP categories}
+#'   \item NICE Guidelines: \href{https://cks.nice.org.uk/topics/hypertension/}{Adult BP categories}
+#'   \item British Heart Foundation: \href{https://www.bhf.org.uk/informationsupport/risk-factors/high-blood-pressure}{Adult BP categories}
+#'   \item NDA Methodology: \href{https://digital.nhs.uk/data-and-information/publications/statistical/national-diabetes-audit/core-report-1-2020-21/methodology}{Adult BP Range}
+#'   \item NPDA Methodology: \href{https://www.rcpch.ac.uk/work-we-do/clinical-audits/npda/transparency-open-data}{Paediatric BP Range}
+#'   \item RCPCH Growth API: \href{https://growth.rcpch.ac.uk/}{Python library for Paediatric SDS calculation}
+#'   \item childsds package: \href{https://mvogel78.r-universe.dev/childsds}{R package for Paediatric SDS calculation}
+#' }
